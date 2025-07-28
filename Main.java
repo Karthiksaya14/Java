84f6dc3c51c1272ffd25b47675b29d44cca640d0
“[Karthik <karthiksaya14@gmail.com>
2025-07-28 21:33:53 +0530

    Added First program

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main{
+    public static void main(String[] args){
+    //This is my first Java program
+
+    // This is a simple Java program that prints a message to the console
+        System.out.println("I think I love Java!");
+    }
+    }
---
+++ variables/var1.java
@@ -0,0 +1,18 @@
+package brocode.variables;
+
+public class var1 {
+    public static void main(String[] args) {
+        
+    
+    int age=18; // This is a variable declaration and initialization
+    int year =2025;
+    int quantity = 10; // This is another variable declaration and initialization
+    int price = 100; // This is a variable declaration and initialization
+
+    System.out.println(age);
+    System.out.println(year);
+    System.out.println(quantity);
+    System.out.println("This year is " + year);
+
+}
+}
